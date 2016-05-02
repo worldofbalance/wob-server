a1d1cc5e119923eaa57c9b1362214352d5490521
kbigleybodlak <hypnothistle@yahoo.com>
2016-05-02 12:07:15 -0700

    added a full game check status of 1, and a reconnection check with a status of 2

--- src/net/Request/RequestPlayInit.java
+++ src/net/Request/RequestPlayInit.java
@@ -11,7 +11,9 @@ import PlayTime.PlayManager;
 import core.GameServer;
 import core.NetworkManager;
 import java.io.IOException;
+import metadata.Constants;
 import net.Response.ResponsePlayInit;
+import net.Response.ResponseSDReconnect;
 import utility.DataReader;
 import utility.Log;
 
@@ -19,7 +21,7 @@ public class RequestPlayInit extends GameRequest {
 
     private int player_id;
     private int room_id;
-    
+    private short status;
     @Override
     public void parse() throws IOException {
         player_id = DataReader.readInt(dataInput);
@@ -28,13 +30,27 @@ public class RequestPlayInit extends GameRequest {
 
     @Override
     public void doBusiness() throws Exception {
-        
+        int tester; //number of players in game test.
         Log.println("Trying to start Play: PlayerID[" +
                 player_id + "], RoomID[" + room_id + "]");
         
-        Play play = PlayManager.getInstance().createPlay(player_id, room_id);
+        //reconnection check.
+        Play test = PlayManager.getInstance().getPlayByPlayerID(player_id);
+        if(test != null){
+            status = 2;
+        }else{
+        //since the play exists, check if it's full.
+        try{
+            tester = PlayManager.getInstance().playerPlayList.get(room_id).getPlayers().size();
+            if(tester == Constants.MAX_NUMBER_OF_PLAYERS){
+                status = 1;
+            }
+        }catch(Exception ex){}
+        }
         
-        play.getPlayer(player_id).setNumber(play.getPlayers().size());
+        Play play = PlayManager.getInstance().createPlay(player_id, room_id);
+        if(status !=2)
+            play.getPlayer(player_id).setNumber(play.getPlayers().size());
         Log.printf("play created, assigning player number %d.", play.getPlayers().size());
         
         Log.println("Trying to start Play: PlayerID[" +
@@ -44,11 +60,12 @@ public class RequestPlayInit extends GameRequest {
             ResponsePlayInit response = new ResponsePlayInit();
             response.setPlayer(play.getPlayer(player_id));
             response.setNumber(play.getPlayer(player_id).getNumber());
+            response.setStatus(status);
             //add response for self
             responses.add(response);
             //get opponent id
             try{
-            int opp_id= PlayManager.manager.getPlayByPlayerID(client.getPlayer().getPlayer_id())
+            int opp_id= PlayManager.getInstance().getPlayByPlayerID(client.getPlayer().getPlayer_id())
                 .getOpponent(client.getPlayer()).getPlayer_id();
             
             /* this is meant for adding a response for all players in a match.
@@ -66,6 +83,8 @@ public class RequestPlayInit extends GameRequest {
         
         
     }
+    
+    
     
 
 
