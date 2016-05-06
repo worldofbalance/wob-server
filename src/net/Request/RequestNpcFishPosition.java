1a55ced89a5571bea978b3095d233601ed9cfeaa
kbigleybodlak <hypnothistle@yahoo.com>
2016-05-05 23:21:26 -0700

    intial addition of the requetNpcFishPosition class, meant to allow the host client to update the other clients in a game using the server

---
+++ src/net/Request/RequestNpcFishPosition.java
@@ -0,0 +1,50 @@
+
+package net.Request;
+
+import PlayTime.PlayManager;
+import core.GameServer;
+import java.io.IOException;
+import java.util.HashMap;
+import model.Prey;
+import net.Response.ResponseNpcFishPosition;
+import utility.DataReader;
+
+/**
+ * an update sent by the host client for movement of NPC fish.
+ * @author Karl
+ */
+public class RequestNpcFishPosition extends GameRequest{
+    private HashMap<Integer, Prey> fishMap; //prey_id -> prey object
+    private int numFish;  //the number of fish in the request.
+    private ResponseNpcFishPosition response;
+    private int p_id;
+    
+    @Override
+    public void parse() throws IOException {
+        numFish = Integer.parseInt(DataReader.readString(dataInput));
+        
+        Prey fish;
+        while(numFish > 0){
+            fish = new Prey();
+            fish.setPrey_id(Integer.parseInt(DataReader.readString(dataInput)));
+            fish.setX(Float.parseFloat(DataReader.readString(dataInput)));
+            fish.setY(Float.parseFloat(DataReader.readString(dataInput)));
+            fish.setRotation(Float.parseFloat(DataReader.readString(dataInput)));
+            fishMap.put(fish.getPrey_id(),fish);
+            numFish--;
+        }
+    }
+
+    @Override
+    public void doBusiness() throws Exception {
+        response = new ResponseNpcFishPosition();
+        response.setNpcFishMap(fishMap);
+        
+        //The playerID of the opponent of the player who sent the request
+        p_id = PlayManager.manager.getPlayByPlayerID(client.getPlayer().getPlayer_id())
+                .getOpponent(client.getPlayer()).getPlayer_id();
+        
+        GameServer.getInstance().getThreadByPlayerID(p_id).send(response);
+    }
+    
+}
