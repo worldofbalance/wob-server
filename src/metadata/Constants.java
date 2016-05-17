c5190140ea4794b569284f5bc13de904546a2bb7
kbigleybodlak <hypnothistle@yahoo.com>
2016-05-17 00:41:24 -0700

    added constant for credit reward

--- src/metadata/Constants.java
+++ src/metadata/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     public static final int Y_MIN = -75;
     public static final int Y_MAX = 75;
     public static final int MIN_PREY = 20;
-    
+    public static final int CREDIT_REWARD = 50;
     
     
     
