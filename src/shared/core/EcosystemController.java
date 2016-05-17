da6d5b4a61cdfbc3468e51b25d4c2255707bfb64
Fito von Zastrow <fito@paragil.com>
2016-05-17 13:33:47 -0700

    Revert "Adds species list to ecosystem response."
    This reverts commit fd55f9728ea421ba1c19cd4366b0c696b7420a2f.

--- src/shared/core/EcosystemController.java
+++ src/shared/core/EcosystemController.java
@@ -254,14 +254,13 @@ public class EcosystemController {
         	setLobby(lobby);
         }
         // Send Ecosystem to Player
-
-        ResponseEcosystem response = new ResponseEcosystem();
-        response.setEcosystem(ecosystem.getID(), ecosystem.getType(), ecosystem.getScore());
-        response.setPlayer(player);
-        response.setZones(zones);
-        response.setEcoSpecies(EcoSpeciesDAO.getSpecies(ecosystem.getID()));
-        NetworkFunctions.sendToPlayer(response, player.getID());
-                
+        if(!Constants.DEBUG_MODE){
+	        ResponseEcosystem response = new ResponseEcosystem();
+	        response.setEcosystem(ecosystem.getID(), ecosystem.getType(), ecosystem.getScore());
+	        response.setPlayer(player);
+	        response.setZones(zones);
+	        NetworkFunctions.sendToPlayer(response, player.getID());
+        }
         // Load Existing Species
         for (Species species : EcoSpeciesDAO.getSpecies(ecosystem.getID())) {
             lobby.getGameEngine().initializeSpecies(species, ecosystem);
