7fed591300a8812d295fc34ba1122ae7a2699ec0
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2016-09-11 14:49:20 -0700

    Lobby should be in lobby folder

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "deploying to thecity.sfsu.edu"
 
 # Deploy lobby server
-rsync -avr ./dist/ wob_wob@thecity.sfsu.edu:~/wob_server
+rsync -avr ./dist/ wob_wob@thecity.sfsu.edu:~/wob_server/lobby
 
 # Deploy lobby server
 rsync -avr ./mini_games/ wob_wob@thecity.sfsu.edu:~/wob_server
