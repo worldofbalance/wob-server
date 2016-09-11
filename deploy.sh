e6f9a7d78975b73003bb65432c45f3390437b514
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2016-09-11 14:46:43 -0700

    Restructured minigames and also deploy them to thecity using Travis

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 echo "deploying to thecity.sfsu.edu"
+
+# Deploy lobby server
 rsync -avr ./dist/ wob_wob@thecity.sfsu.edu:~/wob_server
+
+# Deploy lobby server
+rsync -avr ./mini_games/ wob_wob@thecity.sfsu.edu:~/wob_server
