732b9af721cca3ce34338f7e59a5048df38a785e
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2016-10-17 16:58:25 -0700

    Made some changes to deployment script

--- deploy.sh
+++ deploy.sh
@@ -19,45 +19,45 @@ WOB_WOB
 # now we are back in Travis.
 
 # Deploy lobby server
-rsync -avr ./build/distributions/wob-server/ wob_wob@thecity.sfsu.edu:~/wob_server/lobby
+rsync -avr ./build/distributions/*.tar wob_wob@thecity.sfsu.edu:~/.tmp/
 
-# Deploy lobby server
+# Deploy other servers
 rsync -avr ./mini_games/ wob_wob@thecity.sfsu.edu:~/wob_server
 
 rsync -avr ./scripts/ wob_wob@thecity.sfsu.edu:~/scripts
 
-# log into thecity again, this time to start the JAR Files, everything under this is executed under the WOB_WOB user
-ssh wob_wob@thecity.sfsu.edu <<WOB_WOB
-    cd ~/wob_server
-
-    # Restart all the JAR servers
-
-    # Lobby
-    chmod u+x ~/scripts/start_lobby.sh
-    nohup ~/scripts/start_lobby.sh > ~/logs/lobby.log
-    echo "Lobby Started"
-
-    # Cards of Wild
-    chmod u+x ~/scripts/start_cow.sh
-    nohup ~/scripts/start_cow.sh > ~/logs/cow.log
-    echo "Cards of Wild started"
-
-   # Clash of Species
-    chmod u+x ~/scripts/start_cos.sh
-    nohup ~/scripts/start_cos.sh > ~/logs/cos.log
-    echo "Clash of Species started."
-
-
-    # Running Rhino
-    chmod u+x ~/scripts/start_rno.sh
-    nohup ~/scripts/start_rno.sh > ~/logs/rno.log
-    echo "Running Rhino Started"
-
-    # A Sea Divided
-    chmod u+x ~/scripts/start_sdv.sh
-    nohup ~/scripts/start_sdv.sh > ~/logs/sdv.log
-    echo "A sea divided started"
-WOB_WOB
-
+## log into thecity again, this time to start the JAR Files, everything under this is executed under the WOB_WOB user
+#ssh wob_wob@thecity.sfsu.edu <<WOB_WOB
+#    cd ~/wob_server
+#
+#    # Restart all the JAR servers
+#
+#    # Lobby
+#    chmod u+x ~/scripts/start_lobby.sh
+#    nohup ~/scripts/start_lobby.sh > ~/logs/lobby.log
+#    echo "Lobby Started"
+#
+#    # Cards of Wild
+#    chmod u+x ~/scripts/start_cow.sh
+#    nohup ~/scripts/start_cow.sh > ~/logs/cow.log
+#    echo "Cards of Wild started"
+#
+#   # Clash of Species
+#    chmod u+x ~/scripts/start_cos.sh
+#    nohup ~/scripts/start_cos.sh > ~/logs/cos.log
+#    echo "Clash of Species started."
+#
+#
+#    # Running Rhino
+#    chmod u+x ~/scripts/start_rno.sh
+#    nohup ~/scripts/start_rno.sh > ~/logs/rno.log
+#    echo "Running Rhino Started"
+#
+#    # A Sea Divided
+#    chmod u+x ~/scripts/start_sdv.sh
+#    nohup ~/scripts/start_sdv.sh > ~/logs/sdv.log
+#    echo "A sea divided started"
+#WOB_WOB
+#
 
 
