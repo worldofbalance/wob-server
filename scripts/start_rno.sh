fbc16abd7f00cd20eaa3b156a33d130bbb444b53
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2016-12-08 17:17:32 -0800

    Updated the location for the Java binaries

--- scripts/start_rno.sh
+++ scripts/start_rno.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-printf "\n" | /opt/java8/bin/java -jar ~/wob_server/running_rhino/dist/Speed_Server.jar & printf "\n"
+printf "\n" | /opt/jdk1.8.0_112/bin/java -jar ~/wob_server/running_rhino/dist/Speed_Server.jar & printf "\n"
